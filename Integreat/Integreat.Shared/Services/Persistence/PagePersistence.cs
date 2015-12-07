05a59bd30aecb0e72ddebe9d3983d8379576032d
Daniel Langerenken <daniel.langerenken@gmail.com>
2015-12-07 00:39:12 -0800

    only non-trash pages are shown now ;)

--- Integreat/Integreat.Shared/Services/Persistence/PagePersistence.cs
+++ Integreat/Integreat.Shared/Services/Persistence/PagePersistence.cs
@@ -8,7 +8,7 @@ namespace Integreat.Shared.Services.Persistance
 
 		public Task<List<T>> GetPages<T>(Language language) where T : Page
         {
-            var query = Connection.Table<T>().Where(x => x.LanguageId == language.PrimaryKey);
+            var query = Connection.Table<T>().Where(x => x.LanguageId == language.PrimaryKey && !"trash".Equals(x.Status));
 		    return query.ToListAsync();
         }
 		
