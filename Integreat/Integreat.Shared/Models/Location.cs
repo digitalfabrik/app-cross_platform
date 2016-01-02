6972712cab7cce15be0e100c5ebcc4942f6f3b26
Meash <m4rtinsch@gmail.com>
2016-01-02 19:41:43 +0100

    Remove global field

--- Integreat/Integreat.Shared/Models/Location.cs
+++ Integreat/Integreat.Shared/Models/Location.cs
@@ -7,65 +7,66 @@ using SQLiteNetExtensions.Attributes;
 namespace Integreat.Models
 {
 	public class Location
-    {   
-        [PrimaryKey]
-        [JsonProperty("id")]
-        public int Id{get;set; }
+	{
+		[PrimaryKey]
+		[JsonProperty ("id")]
+		public int Id{ get; set; }
 
-        [JsonProperty("debug")]
-        public bool Debug { get; set; }
+		[JsonProperty ("debug")]
+		public bool Debug { get; set; }
 
-	    public DateTime Modified { get; set; } //TODO
+		public DateTime Modified { get; set; }
+		//TODO
 
-        [JsonProperty("name")]
-        public string Name{get;set; }
+		[JsonProperty ("name")]
+		public string Name{ get; set; }
 
-        [JsonProperty("icon")]
-        public string Icon{get;set; }
+		[JsonProperty ("icon")]
+		public string Icon{ get; set; }
 
-        [JsonProperty("path")]
-        public string Path{get;set; }
+		[JsonProperty ("path")]
+		public string Path{ get; set; }
 
-        [JsonProperty("description")]
-        public string Description{get;set; }
+		[JsonProperty ("description")]
+		public string Description{ get; set; }
 
-        [JsonProperty("global")]
-        public bool Global{get;set; }
+		[JsonProperty ("color")]
+		public string Color{ get; set; }
 
-        [JsonProperty("color")]
-        public string Color{get;set; }
+		[JsonProperty ("cover_image")]
+		public string CityImage{ get; set; }
 
-        [JsonProperty("cover_image")]
-        public string CityImage{get;set; }
+		[JsonProperty ("latitude")]
+		public float Latitude{ get; set; }
 
-        [JsonProperty("latitude")]
-        public float Latitude{get;set; }
+		[JsonProperty ("longitude")]
+		public float Longitude{ get; set; }
 
-        [JsonProperty("longitude")]
-        public float Longitude{get;set;}
+		[OneToMany (CascadeOperations = CascadeOperation.All)]
+		public List<Language> Languages { get; set; }
 
-        [OneToMany(CascadeOperations = CascadeOperation.All)]
-        public List<Language> Languages { get; set; }
+		public Location ()
+		{
+		}
 
-        public Location() { }
-		public Location(int id, string name, string icon, string path, string description, bool global, string color, string cityImage, float latitude, float longitude, bool debug) {
+		public Location (int id, string name, string icon, string path, string description, string color, string cityImage, float latitude, float longitude, bool debug)
+		{
 			Id = id;
 			Name = name;
 			Icon = icon;
 			Path = path;
 			Description = description;
-			Global = global;
 			Color = color;
 			CityImage = cityImage;
 			Latitude = latitude;
 			Longitude = longitude;
-		    Debug = debug;
+			Debug = debug;
 		}
-       
-        public override string ToString()
-        {
-            return Path.Substring(1, Path.Length - 1);
-        }
-    }
+
+		public override string ToString ()
+		{
+			return Path.Substring (1, Path.Length - 1);
+		}
+	}
 }
 
