82a58084a44df515161a5de4e40247c72acd7b66
Meash <m4rtinsch@gmail.com>
2016-01-23 17:39:14 +0100

    Extract no reload timeout as a constant

---
+++ Integreat/Integreat.Shared/Services/NetworkServiceMock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Integreat.Services;
+using System.Threading.Tasks;
+using Integreat.Models;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Integreat.Shared.Models;
+
+namespace Integreat.Shared.Test
+{
+	public class NetworkServiceMock : INetworkService
+	{
+		Task<string> IsServerAlive ()
+			=> Task.Factory.StartNew (() => "some string");
+
+		Task<Collection<Disclaimer>> GetDisclaimers (Language language, Location location, UpdateTime time);
+
+		Task<Collection<Page>> GetPages (Language language, Location location, UpdateTime time);
+
+		Task<HttpResponseMessage> GetPagesDebug (Language language, Location location, UpdateTime time);
+
+		Task<Collection<EventPage>> GetEventPages (Language language, Location location, UpdateTime time);
+
+		Task<Collection<Location>> GetLocations ();
+
+		Task<Collection<Language>> GetLanguages (Location location);
+
+		Task<string> SubscribePush (Location location, string regId);
+
+		Task<string> UnsubscribePush (Location location, string regId);
+	}
+}
+
