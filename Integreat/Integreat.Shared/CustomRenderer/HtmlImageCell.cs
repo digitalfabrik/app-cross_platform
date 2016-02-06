53413b03db1553be7b6543f769daa2f83509e977
Daniel Langerenken <daniel.langerenken@gmail.com>
2016-02-06 02:33:00 -0800

    started customizing imagecell

---
+++ Integreat/Integreat.Shared/CustomRenderer/HtmlImageCell.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.CustomRenderer
+{
+    public class HtmlImageCell : ImageCell
+    {
+    }
+}
