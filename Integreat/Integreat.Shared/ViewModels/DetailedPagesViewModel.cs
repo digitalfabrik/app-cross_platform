10bfe992c01b291e856fcdc0382ee96407b4f3d1
Daniel Langerenken <daniel.langerenken@gmail.com>
2016-09-10 19:27:01 +0200

    Fixed text in detailed page view

--- Integreat/Integreat.Shared/ViewModels/DetailedPagesViewModel.cs
+++ Integreat/Integreat.Shared/ViewModels/DetailedPagesViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Integreat.Shared.Services.Tracking;
-using Xamarin.Forms;
 
 namespace Integreat.Shared.ViewModels
 {
@@ -14,31 +13,13 @@ namespace Integreat.Shared.ViewModels
         }
         
         public string Content { get; set; }
-
-
-        private Command _itemTappedCommand;
-
-        public Command ItemTappedCommand
-        {
-            get { return _itemTappedCommand; }
-            set { SetProperty(ref _itemTappedCommand, value); }
-        }
-
-        private void OnTap(object sender)
-        {
-            var elem = sender as PageViewModel;
-            elem?.ShowPageCommand.Execute(null);
-        }
-
-        public object LastTappedItem { get; set; }
-
+        
         public DetailedPagesViewModel(IAnalyticsService analytics, PageViewModel parentPage, IEnumerable<PageViewModel> pages)
             : base(analytics)
         {
             Title = parentPage.Title;
             _pages = pages;
             Content = parentPage.Page.Content;
-            _itemTappedCommand = new Command(OnTap);
         }
     }
 }
