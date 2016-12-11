97029b47bf0e4f5554194deba49c5c06e0446238
Bernd NK <berndkrupinski@gmail.com>
2016-12-11 14:51:01 +0100

    - Add a couple of test NavigationPages and DetailPages to new ContentContainerPage - Remove ToolbarItem from DetailPage

--- Integreat/Integreat.Shared/Pages/Redesign/ContentContainerPage.xaml.cs
+++ Integreat/Integreat.Shared/Pages/Redesign/ContentContainerPage.xaml.cs
@@ -13,12 +13,38 @@ namespace Integreat.Shared.Pages.Redesign
 		{
 			InitializeComponent ();
             BindingContextChanged += OnBindingContextChanged;
+            CurrentPageChanged += OnCurrentPageChanged;
         }
 
-	    private void OnBindingContextChanged(object sender, EventArgs eventArgs) {
+	    private void OnCurrentPageChanged(object sender, EventArgs eventArgs)
+	    {
+	        
+	    }
+
+	    private async void OnBindingContextChanged(object sender, EventArgs eventArgs) {
             var vm = BindingContext as ContentContainerViewModel;
 	        if (vm == null) return;
             // todo
-	    }
+
+	        await Task.Delay(2000);
+            var navigationPage = new NavigationPage(new DetailPage() {Title = "Globo"});
+            navigationPage.Icon = "globe.png";
+            navigationPage.Title = "Schedule";
+	        await navigationPage.PushAsync(new DetailPage() {Title = "erer"});
+            
+            Children.Add(navigationPage);
+            await Task.Delay(500);
+            navigationPage = new NavigationPage(new DetailPage() {Title = "Search"});
+            navigationPage.Icon = "search.png";
+            navigationPage.Title = "search";
+            Children.Add(navigationPage);
+
+            await Task.Delay(500);
+
+            navigationPage = new NavigationPage(new DetailPage() { Title = "asddasd" });
+            navigationPage.Icon = "search.png";
+            navigationPage.Title = "asdasd";
+            Children.Add(navigationPage);
+        }
 	}
 }
