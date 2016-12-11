a2c19b74dbe59c5a2f4d45a45406326bca7da3f5
Bernd NK <berndkrupinski@gmail.com>
2016-12-11 15:33:32 +0100

    - Update Xamarin.Forms package - Further add test pagess to ContentContainerPage as UWP is experiencing difficulties with NavigationPages inside a TabbedPage

--- Integreat/Integreat.Shared/Pages/Redesign/ContentContainerPage.xaml.cs
+++ Integreat/Integreat.Shared/Pages/Redesign/ContentContainerPage.xaml.cs
@@ -11,26 +11,34 @@ namespace Integreat.Shared.Pages.Redesign
 	public partial class ContentContainerPage : TabbedPage {
 		public ContentContainerPage ()
 		{
-			InitializeComponent ();
+            InitializeComponent();
             BindingContextChanged += OnBindingContextChanged;
             CurrentPageChanged += OnCurrentPageChanged;
         }
 
 	    private void OnCurrentPageChanged(object sender, EventArgs eventArgs)
 	    {
-	        
+	        var asPage = sender as ContentContainerPage;
+	        if (asPage == null) return;
+	        var contentAsNavigationPage = asPage.CurrentPage as NavigationPage;
+	        if (contentAsNavigationPage == null) return;
+            this.ForceLayout();
+	        //NavigationPage.SetHasBackButton(contentAsNavigationPage, true);
 	    }
 
 	    private async void OnBindingContextChanged(object sender, EventArgs eventArgs) {
             var vm = BindingContext as ContentContainerViewModel;
 	        if (vm == null) return;
-            // todo
 
+            ToolbarItems.Add(new ToolbarItem() {Text = "testse", Icon = "globe.png"});
+
+            // todo
 	        await Task.Delay(2000);
-            var navigationPage = new NavigationPage(new DetailPage() {Title = "Globo"});
+            var navigationPage = new NavigationPage(new DetailPage() {Title = "Globo", BackgroundColor = Color.Red});
             navigationPage.Icon = "globe.png";
             navigationPage.Title = "Schedule";
-	        await navigationPage.PushAsync(new DetailPage() {Title = "erer"});
+            
+	        await navigationPage.PushAsync(new DetailPage() {Title = "erer", BackgroundColor = Color.Black});
             
             Children.Add(navigationPage);
             await Task.Delay(500);
