830555b8bbac8d59b7c545429b76185f775d98b2
BerndNK <berndkrupinski@gmail.com>
2017-03-21 10:11:55 +0100

    Closes #77 - Introduce EventDescription property in EventPage which will return the description + date/time as prefix

--- Integreat/Integreat.Shared/Models/EventPage.cs
+++ Integreat/Integreat.Shared/Models/EventPage.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using SQLite.Net.Attributes;
 using Newtonsoft.Json;
 using SQLiteNetExtensions.Attributes;
@@ -26,7 +28,11 @@ namespace Integreat.Shared.Models
 		//[TextBlob("AddressesBlobbed")]
         public List<EventCategory> Categories{ get; set; }
 
-		public EventPage ()
+
+	    public string EventDescription => new DateTime(Event.StartTime).ToString("dd.MM.yyyy HH:mm") + " - " + Description;
+
+
+        public EventPage ()
 		{
 		}
 
