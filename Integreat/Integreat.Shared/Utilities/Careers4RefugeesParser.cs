b0014a37c3a8bd1267b3f6cd177c6562ed9f928a
Ricardo Krause <krr32300@othr.onmicrosoft.com>
2017-05-10 15:52:59 +0200

    remove unused usings

--- Integreat/Integreat.Shared/Utilities/Careers4RefugeesParser.cs
+++ Integreat/Integreat.Shared/Utilities/Careers4RefugeesParser.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
-using System.Xml.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Net.Http;
 using System.Xml.Serialization;
 using Xamarin.Forms;
 
