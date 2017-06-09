c0514f2cfedf049dc6c77b62b6f969d82e908058
Ricardo Krause <krr32300@othr.onmicrosoft.com>
2017-06-09 12:50:08 +0200

    rename class to fit c4r style

--- Integreat/Integreat.Shared/Models/Extras/Sprungbrett/SprungbrettJobOffer.cs
+++ Integreat/Integreat.Shared/Models/Extras/Sprungbrett/SprungbrettJobOffer.cs
@@ -9,7 +9,7 @@ namespace Integreat.Shared.Models.Extras.Sprungbrett
     public class SprungbrettJobOffer : JobOfferBase
     {
         [JsonProperty("title")]
-        public string Title { get; set; }
+        public string JobTitle { get; set; }
         [JsonProperty("apprenticeship")]
         public string Apprenticeship { get; set; }
         [JsonProperty("employment")]
