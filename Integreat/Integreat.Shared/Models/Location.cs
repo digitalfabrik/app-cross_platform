05536b5043d0bc1ce961c68ef92eeabd493afeb6
Ricardo Krause <krr32300@othr.onmicrosoft.com>
2017-10-14 10:18:24 +0200

    remove test comments

--- Integreat/Integreat.Shared/Models/Location.cs
+++ Integreat/Integreat.Shared/Models/Location.cs
@@ -68,15 +68,11 @@ namespace Integreat.Shared.Models
 
         [JsonProperty("ige-ihk-apprenticeships")]
         public string IhkApprenticeshipsExtras { get; set; }
-        //for testing
-        //public string IhkApprenticeshipsExtras => "{\"enabled\": 1, \"url\": \"https://www.ihk-lehrstellenboerse.de/joboffers/search.html?hitsPerPage=100&page=0&sortColumn=-1&sortDir=asc&query=-1&organisationName=Unternehmen+eingeben&status=1&mode=1&dateTypeSelection=PUB_DATE&location=N%C3%BCrnberg%2C+Mittelfranken&thisYear=true&nextYear=true&distance=50\"}";
         public string IhkApprenticeshipsEnabled => IsEnabledSafe(IhkApprenticeshipsExtras);
         public string IhkApprenticeshipsUrl => UrlOrEmptyString(IhkApprenticeshipsExtras);
 
         [JsonProperty("ige-ihk-internships")]
         public string IhkInternshipsExtras { get; set; }
-        //for testing
-        //public string IhkInternshipsExtras => "{\"enabled\": 1, \"url\": \"https://www.ihk-lehrstellenboerse.de/joboffers/searchTrainee.html?location=München&distance=0 \"}";
         public string IhkInternshipsEnabled => IsEnabledSafe(IhkInternshipsExtras);
         public string IhkInternshipsUrl => UrlOrEmptyString(IhkInternshipsExtras);
 
