a16150da71a5c06165b7dbef587d1d5c77274564
Simple-codinger <vincentgierisch@gmail.com>
2017-11-14 00:40:39 +0100

    small improvements

--- Integreat/Integreat.Shared/IntegreatApp.xaml.cs
+++ Integreat/Integreat.Shared/IntegreatApp.xaml.cs
@@ -9,11 +9,16 @@ using Integreat.Shared.Converters;
 using Integreat.Shared.Models;
 using Integreat.Shared.Utilities;
 using Integreat.Utilities;
+using Integreat.Shared.Data.Services;
+using Integreat.Shared.Data;
+using System.Collections.Generic;
+using Integreat.Shared.Data.Loader;
+using Integreat.Shared.Data.Loader.Targets;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Integreat.Shared
 {
-    [SecurityCritical]
+    //[SecurityCritical]
     public partial class IntegreatApp : Application
     {
         [SecurityCritical]
@@ -32,9 +37,26 @@ namespace Integreat.Shared
 
             string[] segments = uri.Segments.Where(s => s != "/").ToArray().Select(s => s.Trim(new Char[] { '/' })).ToArray();
 
+
+
+            //get location
+            
+
             //webapp url to cms url
 
             base.OnAppLinkRequestReceived(uri);
+        }
+
+        private async Task<Location> getLocation(string shortname)
+        {
+            //create dataloader
+            LocationsDataLoader dataloader = new LocationsDataLoader(DataLoadServiceFactory.Create());
+
+            List<Location> locations = new List<Location>(await dataloader.Load(false, null));
+
+            Location location = locations.Where(l => l.Path == "/" + shortname + "/").First();
+
+            return location;
         }
     }
 }
