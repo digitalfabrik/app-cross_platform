3da2b8806fb6c089c672bd23d6e30507501a9d25
Ricardo Krause <kaser23@gmx.de>
2017-11-21 13:26:18 +0100

    Remove the Redesign Folders and cleanup code

---
+++ Integreat/Integreat.Shared/Pages/ExtrasContentPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Security;
+
+namespace Integreat.Shared.Pages
+{
+    /// <summary>
+    /// ToDo merge this class with maincontent Page, they are identical
+    /// </summary>
+    public partial class ExtrasContentPage
+	{
+	    [SecurityCritical]
+        public ExtrasContentPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
