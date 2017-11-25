b8b08e9eb4aee381643e21c47d72a4dd19b9eb3b
Ricardo Krause <kaser23@gmx.de>
2017-11-25 07:54:22 +0100

    format and comment class

--- Integreat/Integreat.iOS/Effects/NoScrollListViewEffect.cs
+++ Integreat/Integreat.iOS/Effects/NoScrollListViewEffect.cs
@@ -5,17 +5,26 @@ using Xamarin.Forms.Platform.iOS;
 
 [assembly: ResolutionGroupName("Integreat")]
 [assembly: ExportEffect(typeof(NoScrollListViewEffect), "NoScrollEffect")]
-namespace Integreat.iOS.Effects {
-    public class NoScrollListViewEffect : PlatformEffect {
-        private UITableView _NativeList => Control as UITableView;
+namespace Integreat.iOS.Effects
+{
+    /// <summary>
+    /// This Plattform effect disables the scroll functionality for a UITableView control
+    /// </summary>
+    /// <seealso cref="Xamarin.Forms.Platform.iOS.PlatformEffect" />
+    public class NoScrollListViewEffect : PlatformEffect
+    {
+        private UITableView NativeList => Control as UITableView;
 
-        protected override void OnAttached() {
-            if (_NativeList != null) {
-                _NativeList.ScrollEnabled = false;
+        protected override void OnAttached()
+        {
+            if (NativeList != null)
+            {
+                NativeList.ScrollEnabled = false;
             }
         }
 
-        protected override void OnDetached() {
+        protected override void OnDetached()
+        {
         }
     }
 }
