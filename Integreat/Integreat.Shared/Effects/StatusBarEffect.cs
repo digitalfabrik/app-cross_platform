ae0435e19f3002f04bd1e0f6f7025339bf99c8a0
Simple-codinger <vincentgierisch@gmail.com>
2017-11-26 02:04:24 +0100

    add StatusBarEffect class

---
+++ Integreat/Integreat.Shared/Effects/StatusBarEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Effects
+{
+    public class StatusBarEffect : RoutingEffect
+    {
+        private static Color _backgroundColor;
+
+        public StatusBarEffect() : base("Xamarin.StatusBarEffect") { }
+
+        public static Color BackgroundColor { get => _backgroundColor; set => _backgroundColor = value; }
+    }
+}
