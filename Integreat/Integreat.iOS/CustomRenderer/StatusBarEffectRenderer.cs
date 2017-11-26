dafe93cfea18c658fdb7424d366ea3cf613370ba
Simple-codinger <vincentgierisch@gmail.com>
2017-11-26 02:38:37 +0100

    add ios StatusBarEffectRenderer

---
+++ Integreat/Integreat.iOS/CustomRenderer/StatusBarEffectRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Integreat.Shared.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(StatusBarEffect), "StatusBarEffect")]
+namespace Integreat.iOS.CustomRenderer
+{
+    /// <summary>
+    /// This Class is used to render the status bar effect 
+    /// </summary>
+    public class StatusBarEffectRenderer:PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            UIView statusBar = UIApplication.SharedApplication.ValueForKey(new Foundation.NSString("statusBar")) as UIView;
+            if(statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor")))
+            {
+                statusBar.BackgroundColor = StatusBarEffect.BackgroundColor.ToUIColor();
+            }
+        }
+
+        protected override void OnDetached(){}
+    }
+}
