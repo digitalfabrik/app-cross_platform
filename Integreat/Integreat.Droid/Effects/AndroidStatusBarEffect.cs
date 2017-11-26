083aedcebf87b05df6b0392ef6fe0ead5ddb449a
Simple-codinger <vincentgierisch@gmail.com>
2017-11-26 20:00:50 +0100

    Add statusbareffect in integrations

---
+++ Integreat/Integreat.Droid/Effects/AndroidStatusBarEffect.cs
@@ -0,0 +1,26 @@
+﻿using Android.Views;
+using Integreat.Droid.CustomRenderer;
+using Integreat.Droid.Helpers;
+using Integreat.Shared.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("Integreat")]
+[assembly: ExportEffect(typeof(AndroidStatusBarEffect), "StatusBarEffect")]
+namespace Integreat.Droid.CustomRenderer
+{
+    /// <summary>
+    /// This Class is used to render the status bar effect 
+    /// </summary>
+    public class AndroidStatusBarEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            var backgroundColor = StatusBarEffect.GetBackgroundColor().ToAndroid();
+            Window window = Globals.Window;
+            window.SetStatusBarColor(backgroundColor);
+        }
+
+        protected override void OnDetached(){}
+    }
+}
---
+++ Integreat/Integreat.iOS/Effects/IOsStatusBarEffect.cs
@@ -0,0 +1,26 @@
+﻿using Integreat.iOS.CustomRenderer;
+using Integreat.Shared.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(IosStatusBarEffect), "StatusBarEffect")]
+namespace Integreat.iOS.CustomRenderer
+{
+    /// <summary>
+    /// This class is used to render the status bar effect 
+    /// </summary>
+    public class IosStatusBarEffect:PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            UIView statusBar = UIApplication.SharedApplication.ValueForKey(new Foundation.NSString("statusBar")) as UIView;
+            if(statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
+            {
+                statusBar.BackgroundColor = StatusBarEffect.GetBackgroundColor().ToUIColor();
+            }
+        }
+
+        protected override void OnDetached(){}
+    }
+}
