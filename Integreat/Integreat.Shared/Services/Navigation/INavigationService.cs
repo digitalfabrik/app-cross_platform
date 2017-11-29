100e6ee4c911df8c7c0f814d815e6fa000dc9ac9
Simple-codinger <vincentgierisch@gmail.com>
2017-11-29 22:49:36 +0100

    add INavigationService

---
+++ Integreat/Integreat.Shared/Services/Navigation/INavigationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Integreat.Shared.ViewFactory;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Services.Navigation
+{
+    /// <summary>
+    /// Interface for navigation service
+    /// </summary>
+    public interface INavigationService
+    {
+        Task<IViewModel> PopModalAsync();
+
+        Task PopToRootAsync();
+
+        Task<TViewModel> PushAsync<TViewModel>(TViewModel viewModel)
+            where TViewModel : class, IViewModel;
+    }
+}
