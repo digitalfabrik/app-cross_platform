d6ba4a5ede8fcc8f677ff3303a7170862df9c734
Simple-codinger <vincentgierisch@gmail.com>
2018-01-13 02:55:40 +0100

    add IIDService

---
+++ Integreat/Integreat.Droid/Services/IntegreatFirebaseIIDService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+
+namespace Integreat.Droid.Services
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class IntegreatFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "IntegreatFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            System.Diagnostics.Debug.WriteLine("Refreshed token: "+refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+
+        }
+
+        private void SendRegistrationToServer(string token){
+            
+        }
+    }
+}
