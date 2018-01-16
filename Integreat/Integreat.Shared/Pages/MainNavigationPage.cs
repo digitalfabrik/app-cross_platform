4ffa25fdfccc458b8e0e24abaddd4e8b516c5f30
Simple-codinger <vincentgierisch@gmail.com>
2018-01-16 20:30:10 +0100

    Add MainNavigationPage

---
+++ Integreat/Integreat.Shared/Pages/MainNavigationPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Pages
+{
+    public class MainNavigationPage : NavigationPage
+    {
+        public MainNavigationPage()
+        {
+
+        }
+    }
+}
+
