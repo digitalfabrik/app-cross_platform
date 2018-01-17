4903b2bc38218ab873abd7e977663a62abea8e8f
Ricardo Krause <kaser23@gmx.de>
2018-01-17 20:00:37 +0100

    remove unused command

--- Integreat/Integreat.Shared/Pages/General/GeneralWebViewPage.xaml.cs
+++ Integreat/Integreat.Shared/Pages/General/GeneralWebViewPage.xaml.cs
@@ -15,12 +15,6 @@ namespace Integreat.Shared.Pages.General
     [SecurityCritical]
     public partial class GeneralWebViewPage
     {
-        /// <summary>
-        /// Gets or sets the on navigating command, which will be executed when the user navigates within the webView (clicks on a link).
-        /// </summary>
-        // ReSharper disable once UnusedAutoPropertyAccessor.Local
-        private Command OnNavigatingCommand { get; set; }
-
         [SecurityCritical]
         public GeneralWebViewPage()
         {
@@ -34,7 +28,6 @@ namespace Integreat.Shared.Pages.General
         /// <param name="e">The <see cref="WebNavigatingEventArgs"/> instance containing the event data.</param>
         private void WebView_OnNavigating(object sender, WebNavigatingEventArgs e)
         {
-            OnNavigatingCommand?.Execute(e);
             // try to give the OnNavigating event the ViewModel of this WebPage
             ((BaseWebViewViewModel)BindingContext)?.OnNavigating(e);
         }
