64a64e2077a398c6fd4a95a764c5e9763e51c145
Simple-codinger <vincentgierisch@gmail.com>
2018-01-17 01:29:37 +0100

    add constructor

--- Integreat/Integreat.Shared/Pages/MainNavigationPage.cs
+++ Integreat/Integreat.Shared/Pages/MainNavigationPage.cs
@@ -4,12 +4,12 @@ using Xamarin.Forms;
 
 namespace Integreat.Shared.Pages
 {
+    /// <summary>
+    /// Main navigation page. This class is just a wrapper. So that the renderer doesn't have to render a xamarin base class
+    /// </summary>
     public class MainNavigationPage : NavigationPage
     {
-        public MainNavigationPage()
-        {
-
-        }
+        public MainNavigationPage(Page root) : base(root){}
     }
 }
 
