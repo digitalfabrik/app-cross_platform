b9078ce1eac9d9a1971f5ab39b269d358dca8708
Simple-codinger <vincentgierisch@gmail.com>
2018-01-17 02:17:13 +0100

    Add ViewDidLayoutSubviews

--- Integreat/Integreat.iOS/CustomRenderer/MainNavigationPageRenderer.cs
+++ Integreat/Integreat.iOS/CustomRenderer/MainNavigationPageRenderer.cs
@@ -14,14 +14,48 @@ namespace Integreat.iOS.CustomRenderer
     /// </summary>
     public class MainNavigationPageRenderer : NavigationRenderer
     {
-  
+
         public override void ViewWillAppear(bool animated)
         {
             var toolbar = View.Subviews.OfType<UIToolbar>().FirstOrDefault(v => v.GetType() != typeof(UIToolbar));
-            if(toolbar != null){
+            if (toolbar != null)
+            {
                 toolbar.RemoveFromSuperview();
             }
             base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            UIView[] subviews = View.Subviews.Where(v => v != NavigationBar).ToArray();
+            var toolBarViews = subviews.Where(v => v is UIToolbar).ToArray();
+            var otherViews = subviews.Where(v => !(v is UIToolbar)).ToArray();
+
+            nfloat toolBarHeight = 0;
+
+            foreach(var uiView in toolBarViews){
+                uiView.SizeToFit();
+                uiView.Frame = new CoreGraphics.CGRect
+                {
+                    X = 0,
+                    Y = View.Bounds.Height - uiView.Frame.Height,
+                    Width = View.Bounds.Width,
+                    Height = uiView.Frame.Height
+                };
+
+                var thisToolBarHeight = uiView.Frame.Height;
+                if(toolBarHeight < thisToolBarHeight){
+                    toolBarHeight = thisToolBarHeight;
+                }
+            }
+
+            var othersHeight = View.Bounds.Height - toolBarHeight;
+            var othersFrame = new CoreGraphics.CGRect(View.Bounds.X, View.Bounds.Y, View.Bounds.Width, othersHeight);
+
+            foreach(var uiView in otherViews){
+                uiView.Frame = othersFrame;
+            }
         }
     }
 }
