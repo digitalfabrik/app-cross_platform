9685034a5596515b17044bd41a2aaf778b19344a
Simple-codinger <vincentgierisch@gmail.com>
2018-01-23 20:07:49 +0100

    add FirebaseCloudMessaging

---
+++ Integreat/Integreat.Shared/Firebase/FirebaseCloudMessaging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Firebase
+{
+    public class FirebaseCloudMessaging
+    {
+        private static IFirebasePushNotificationManager _instance;
+        
+        public static IFirebasePushNotificationManager Current
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = DependencyService.Get<IFirebasePushNotificationManager>();
+
+                return _instance;
+            }
+        }
+    }
+}
