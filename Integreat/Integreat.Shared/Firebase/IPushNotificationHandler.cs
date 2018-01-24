2660e75d1ab3a5a63c3b28093aa5118dd4054cf1
Simple-codinger <vincentgierisch@gmail.com>
2018-01-25 00:32:36 +0100

    add pushnotificationhandler interface

--- Integreat/Integreat.Shared/Firebase/IPushNotificationHandler.cs
+++ Integreat/Integreat.Shared/Firebase/IPushNotificationHandler.cs
@@ -1,7 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Integreat.Shared.Firebase
 {
     public interface IPushNotificationHandler
     {
+        /// <summary>
+        /// triggered when error occurs.
+        /// </summary>
+        /// <param name="error">Error.</param>
+        void OnError(string error);
+        /// <summary>
+        /// triggered when a notification is opened
+        /// </summary>
+        void OnOpened();
+        /// <summary>
+        /// triggered when a notification is received
+        /// </summary>
+        /// <param name="parameters">Parameters.</param>
+        void OnReceived(IDictionary<string, object> parameters);
     }
 }
