9c36e3277b60dfb994cad6a1e3cdad7830e6267b
Simple-codinger <vincentgierisch@gmail.com>
2018-01-29 23:17:47 +0100

    remove Todo

--- Integreat/Integreat.Droid/Services/IntegreatFirebaseIIDService.cs
+++ Integreat/Integreat.Droid/Services/IntegreatFirebaseIIDService.cs
@@ -7,20 +7,18 @@ namespace Integreat.Droid.Services
     [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
     public class IntegreatFirebaseIidService : FirebaseInstanceIdService
     {
-        //ToDo is this done ?? this string is not used
-        const string TAG = "IntegreatFirebaseIIDService";
         public override void OnTokenRefresh()
         {
             var refreshedToken = FirebaseInstanceId.Instance.Token;
-            System.Diagnostics.Debug.WriteLine("Refreshed topic: " + refreshedToken);
+            System.Diagnostics.Debug.WriteLine("Refreshed token: " + refreshedToken);
             SendRegistrationToServer(refreshedToken);
 
         }
 
-        private static void SendRegistrationToServer(string topic)
+        private static void SendRegistrationToServer(string token)
         {
             //send topic to cms (maybe)
-            FirebasePushNotificationManager.SaveTopicToken(topic);
+            FirebasePushNotificationManager.SaveToken(token);
         }
     }
 }
