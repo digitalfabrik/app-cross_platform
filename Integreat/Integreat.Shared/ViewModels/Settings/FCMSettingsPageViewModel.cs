6e2f0e5063e8875727421e9ce3ba973574414534
Simple-codinger <vincentgierisch@gmail.com>
2018-01-30 03:00:04 +0100

    Build main messaging settings

--- Integreat/Integreat.Shared/ViewModels/Settings/FCMSettingsPageViewModel.cs
+++ Integreat/Integreat.Shared/ViewModels/Settings/FCMSettingsPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows.Input;
+using Integreat.Shared.Data.Loader;
 using Integreat.Shared.Firebase;
+using Integreat.Shared.Models;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace Integreat.Shared.ViewModels
 {
@@ -9,41 +13,107 @@ namespace Integreat.Shared.ViewModels
     /// <summary>
     /// Class FCMSettingsPageViewModel contains all information and functionality for the  FCMSettingsPage
     /// </summary>
-    public class FcmSettingsPageViewModel:BaseViewModel
+    //ToDo: replace all strings with App.Resoure
+    public class FcmSettingsPageViewModel : BaseContentViewModel
     {
-        private string _topics;
+        private bool _isTopicEnabled;
+        private string _TopicText;
+        private string _TopicsText;
 
-        public FcmSettingsPageViewModel()
+        public FcmSettingsPageViewModel(DataLoaderProvider dataLoaderProvider) : base(dataLoaderProvider)
         {
-            AddTopicCommand = new Command(TopicCommand);
-            UnsubscribeAllCommand = new Command(UnsubscribeAll);
+            OpenTopicsCommand = new Command(OpenTopics);
+            _isTopicEnabled = false;
+            Title = "Messaging Settings";
         }
 
-        public string TopicText { get; set; }
-
-        public string Topics { get => _topics; private set => SetProperty(ref _topics, value); }
-
-        public ICommand AddTopicCommand { get; }
-        public ICommand UnsubscribeAllCommand { get; }
-
-
-        public void TopicCommand(object obj)
+        public string TopicText 
         {
-            if (TopicText.IsNullOrEmpty()) return;
-            FirebaseCloudMessaging.Current.Subscribe(TopicText);
-            RefreshTopics();
+            get => _TopicText; 
+
+            private set
+            {
+                SetProperty(ref _TopicText, value);
+                OnPropertyChanged(nameof(TopicText));
+            }
         }
 
-        public void UnsubscribeAll(object obj)
+        public string ExplanationText => "To subscribe to another topic, just switch the location and/or the language";
+
+        public string TopicsText 
         {
-            FirebaseCloudMessaging.Current.UnsubscribeAll();
-            RefreshTopics();
+            get => _TopicsText; 
+
+            private set
+            {
+                SetProperty(ref _TopicsText, value);
+                OnPropertyChanged(nameof(TopicsText));
+            }
         }
 
-        private void RefreshTopics()
+        public bool IsTopicEnabled 
         {
-            var topics = FirebaseCloudMessaging.Current.SubscribedTopics.Aggregate(string.Empty, (current, topic) => current + "|" + topic);
-            Topics = topics;
+            get => _isTopicEnabled; 
+
+            set
+            {
+                if(IsTopicEnabled !=value)
+                {
+                    //ToDo: subscribe to topic
+                    if (value)
+                        FirebaseCloudMessaging.Current.Subscribe(BuildTopicString());
+                    else
+                        FirebaseCloudMessaging.Current.Unsubscribe(BuildTopicString());
+
+                    RefreshTopicsText();
+                    _isTopicEnabled = value;
+                    OnPropertyChanged(nameof(IsTopicEnabled));
+                }
+            }
+        }
+
+        public ICommand OpenTopicsCommand { get; }
+
+        public override void OnAppearing()
+        {
+            RefreshTopicText();
+            RefreshTopicsText();
+            RefreshSwitch();
+            base.OnAppearing();
+        }
+
+        private void OpenTopics(object obj)
+        {
+            //Todo
+        }
+
+        private void RefreshTopicText()
+        {
+            TopicText = "Get Notifications for: " + LastLoadedLocation.Name + "(" + LastLoadedLanguage.ShortName + ")";
+        }
+
+        private void RefreshTopicsText()
+        {
+            TopicsText = "Edit " + FirebaseCloudMessaging.Current.SubscribedTopics.Count().ToString() + " subscriptions";
+        }
+
+
+        private void RefreshSwitch()
+        {
+            if (FirebaseCloudMessaging.Current.SubscribedTopics.IndexOf(BuildTopicString()) > -1)
+                IsTopicEnabled = true;
+            else
+                IsTopicEnabled = false;
+        }
+
+        private string BuildTopicString()
+        {
+            return LastLoadedLocation + "-" + LastLoadedLanguage.ShortName + "-news";
+        }
+
+        protected override void LoadContent(bool forced = false, Language forLanguage = null, Location forLocation = null)
+        {
+            RefreshTopicText();
         }
     }
 }
