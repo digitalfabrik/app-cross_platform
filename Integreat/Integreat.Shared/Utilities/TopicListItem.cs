713f31d1c16b2cf502b539fcfec27f534d4cb705
Simple-codinger <vincentgierisch@gmail.com>
2018-01-31 03:35:59 +0100

    add GenerateDisplayName function

--- Integreat/Integreat.Shared/Utilities/TopicListItem.cs
+++ Integreat/Integreat.Shared/Utilities/TopicListItem.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Linq;
+using Integreat.Shared.Data.Loader;
+using Integreat.Shared.Models;
+
 namespace Integreat.Shared.Utilities
 {
     public class TopicListItem
     {
-        public TopicListItem()
+        private DataLoaderProvider _dataLoaderProvider;
+
+        public TopicListItem(string topicString, DataLoaderProvider dataLoaderProvider)
         {
+            _dataLoaderProvider = dataLoaderProvider;
+            TopicString = topicString;
+        }
+
+        public string DisplayName;
+
+        public string TopicString;
+
+        private string GenerateDisplayName(string topicString)
+        {
+            string[] temp = topicString.Split('-');
+
+            Location location = _dataLoaderProvider.LocationsDataLoader.Load(false).Result.First(l => l.Id == Int32.Parse(temp[0]));
+
+            string displayName;
+
+            if(temp[2] == "news")
+            {
+                //default topic
+                displayName = location.Name + "(" + temp[1] + ")";
+            }
+            else
+            {
+                displayName = location.Name + "(" + temp[1] + ") - " + temp[2];
+            }
+
+            return displayName;
         }
     }
 }
