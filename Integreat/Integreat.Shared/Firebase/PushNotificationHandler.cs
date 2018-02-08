3990c58be5cfa7f52d68c0212fa85a448e4fd3a4
Simple-codinger <vincentgierisch@gmail.com>
2018-02-08 01:47:13 +0100

    add PushNotificationHandler

---
+++ Integreat/Integreat.Shared/Firebase/PushNotificationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Integreat.Shared.Firebase
+{
+    public class PushNotificationHandler:IPushNotificationHandler
+    {
+        public void OnError(string error)
+        {
+            Debug.WriteLine("Error receiving a message: " + error);
+        }
+
+        public void OnOpened()
+        {
+            Debug.WriteLine("Message opened");
+        }
+
+        public void OnReceived(IDictionary<string, object> parameters)
+        {
+            Debug.WriteLine("Message received");
+        }
+    }
+}
