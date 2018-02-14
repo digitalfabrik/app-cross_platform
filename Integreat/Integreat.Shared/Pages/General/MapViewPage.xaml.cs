d9ebff51be0185a0c16e45a55fdc846502d8cf90
Simple-codinger <vincentgierisch@gmail.com>
2018-02-14 13:11:14 +0100

    add MapViewPage

---
+++ Integreat/Integreat.Shared/Pages/General/MapViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Pages.General
+{
+    public partial class MapViewPage
+    {
+        public MapViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
