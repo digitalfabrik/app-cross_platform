127f1b6478011d198ebba1f4b3ea308621e9331d
Simple-codinger <vincentgierisch@gmail.com>
2018-02-14 12:46:13 +0100

    add MapsContentView

---
+++ Integreat/Integreat.Shared/Views/MapsContentView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Views
+{
+    /// <summary>
+    /// Custom Map View
+    /// </summary>
+    public class MapsContentView:View
+    {
+        public static readonly BindableProperty IndeterminateProperty = BindableProperty.Create<MapsContentView, bool>(prop => prop.Changed, default(bool));
+
+        public bool Changed
+        {
+            get { return (bool)GetValue(IndeterminateProperty); }
+            set
+            {
+                SetValue(IndeterminateProperty, value);
+                OnPropertyChanged();
+            }
+        }
+    }
+}
