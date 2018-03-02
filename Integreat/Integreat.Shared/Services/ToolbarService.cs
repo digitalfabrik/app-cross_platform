e9aa54cfa4c348760067e8c0461441a73ce51721
Simple-codinger <vincentgierisch@gmail.com>
2018-03-02 14:20:37 +0100

    add ToolbarItem property

--- Integreat/Integreat.Shared/Services/ToolbarService.cs
+++ Integreat/Integreat.Shared/Services/ToolbarService.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Integreat.Shared.Pages;
+using Xamarin.Forms;
+
 namespace Integreat.Shared.Services
 {
     public class ToolbarService
     {
+        private ObservableCollection<ToolbarItem> _toolbarItems;
+        private MainNavigationPage _mainNavPage;
+
         public ToolbarService()
         {
+            _toolbarItems = new ObservableCollection<ToolbarItem>();
+            _mainNavPage = (MainNavigationPage)Application.Current.MainPage;
+
+            _toolbarItems.CollectionChanged += ToolbarItemsChanged;
+        }
+
+        public IList<ToolbarItem> ToolbarItems { get => _toolbarItems; }
+
+        private void ToolbarItemsChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+
         }
     }
 }
