188e62f5597e50df35d786caf72bb24716487500
Simple-codinger <vincentgierisch@gmail.com>
2018-03-10 02:11:25 +0100

    add IntegreatToolbarItem

---
+++ Integreat/Integreat.Shared/Utilities/IntegreatToolbarItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Utilities
+{
+    public class IntegreatToolbarItem : ToolbarItem
+    {
+        private readonly string _identifier;
+
+        public IntegreatToolbarItem()
+        {
+            
+        }
+    }
+}
