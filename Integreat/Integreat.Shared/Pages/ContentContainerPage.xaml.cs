781692e66d08f6c89b1173c2df510c651ed5a7f3
Simple-codinger <vincentgierisch@gmail.com>
2018-03-11 01:19:50 +0100

    remove unused function

--- Integreat/Integreat.Shared/Pages/ContentContainerPage.xaml.cs
+++ Integreat/Integreat.Shared/Pages/ContentContainerPage.xaml.cs
@@ -40,63 +40,8 @@ namespace Integreat.Shared.Pages
             _vm.CreateMainView(Children);
 
             CurrentPage = Children[1];
-
-            // when the current child (Extras, Main, Events) is changed,
-            CurrentPageChanged += (o, args) => UpdateToolbarItems();
-
-            // initial toolbar initialization after creating the main view
-            UpdateToolbarItems();
         }
 
-
-        /// <summary>
-        /// Updates the toolbar items.
-        /// </summary>
-        private void UpdateToolbarItems()
-        {
-            var activeChild = CurrentPage;
-            if (!(activeChild.BindingContext is BaseContentViewModel)) return;
-
-            // get the toolbar items of the current child and the default items, merge them and set them to be shown
-            try
-            {
-                // the following three statements are all MVVM breaking, structure assuming calls, so we try/catch it just in case
-                var childItems = ((BaseContentViewModel)activeChild.BindingContext).ToolbarItems;
-                var defaultItems = ((ContentContainerViewModel)BindingContext).DefaultToolbarItems;
-                var navigationPage = (MainNavigationPage)Application.Current.MainPage;
-
-                // current shown page
-                var crntPage = navigationPage.CurrentPage;
-
-                // clear the current items
-                navigationPage.ToolbarItems.Clear();
-#if __IOS__
-                ToolbarItems.Clear();
-#endif
-                // add the child items only if the current shown page is the contentContainer
-                if (childItems != null && crntPage == this)
-                    navigationPage.ToolbarItems.AddRange(childItems);
-
-                // add the default items
-                if (defaultItems != null)
-                {
-#if __IOS__
-                    foreach(ToolbarItem item in defaultItems)
-                    {
-                        if(item.Order == ToolbarItemOrder.Primary){
-                            ToolbarItems.Add(item);
-                        }
-                    }
-#else
-                    navigationPage.ToolbarItems.AddRange(defaultItems);
-#endif
-                }
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-        }
 
         [SecurityCritical]
         private void OnBindingContextChanged(object sender, EventArgs eventArgs)
