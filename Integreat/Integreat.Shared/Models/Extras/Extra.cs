7afafce19a4a8e3cc7765a5f47bafea3613090dc
Simple-codinger <vincentgierisch@gmail.com>
2018-06-09 23:00:44 +0200

    move Extra model

---
+++ Integreat/Integreat.Shared/Models/Extras/Extra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Integreat.Shared.Models.Extras
+{
+    public class Extra
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("alias")]
+        public string Alias { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
