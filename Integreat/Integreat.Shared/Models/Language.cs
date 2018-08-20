c80d25a401b45475870b8b452d973e42b7328104
Simple-codinger <vincentgierisch@gmail.com>
2018-08-20 02:51:25 +0200

    change language to v3

--- Integreat/Integreat.Shared/Models/Language.cs
+++ Integreat/Integreat.Shared/Models/Language.cs
@@ -12,26 +12,21 @@ namespace Integreat.Shared.Models
         [JsonProperty ("id")]
 		public int Id{ get; set; }
 
-		public DateTime Modified { get; set; }
+        [JsonProperty("code")]
+        public string ShortName { get; set; }
 
-		[JsonProperty ("code")]
-		public string ShortName{ get; set; }
+        [JsonProperty("native_name")]
+        public string Name { get; set; }
 
-		[JsonProperty ("native_name")]
-		public string Name{ get; set; }
+        [JsonProperty("country_flag_url")]
+        public string IconPath { get; set; }
 
-		/// <summary>
-		/// Gets or sets the read direction for the language.
-		/// </summary>
-		[JsonProperty ("dir")]
-		public string Direction { get; set; }
-
-		[JsonProperty ("country_flag_url")]
-		public string IconPath{ get; set; }
+        [JsonProperty("dir")]
+        public string Direction { get; set; }
 
         public Location Location { get; set; }
-        public string PrimaryKey { get; set; }
 
+        public string PrimaryKey { get; set; }
 
         public override string ToString ()
 		{
