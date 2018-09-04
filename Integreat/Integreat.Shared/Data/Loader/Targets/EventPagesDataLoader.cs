4f89d7e689395ae1b0d019ab40e789c8708c82bb
Simple-codinger <vincentgierisch@gmail.com>
2018-09-04 02:32:14 +0200

    remoce persistWorker

--- Integreat/Integreat.Shared/Data/Loader/Targets/EventPagesDataLoader.cs
+++ Integreat/Integreat.Shared/Data/Loader/Targets/EventPagesDataLoader.cs
@@ -10,7 +10,7 @@ namespace Integreat.Shared.Data.Loader.Targets
     /// <inheritdoc />
     public class EventPagesDataLoader : IDataLoader
     {
-        public const string FileNameConst = "eventsV1";
+        public const string FileNameConst = "eventsV3";
 
         /// <inheritdoc />
         public string FileName => FileNameConst;
@@ -51,27 +51,12 @@ namespace Integreat.Shared.Data.Loader.Targets
                 foreach (var page in pages)
                 {
                     page.PrimaryKey = Page.GenerateKey(page.Id, forLocation, forLanguage);
-                    if (!"".Equals(page.ParentJsonId) && page.ParentJsonId != null)
-                    {
-                        page.ParentId = Page.GenerateKey(page.ParentJsonId, forLocation, forLanguage);
-                    }
-                }
-            };
-
-            // action which will be executed on the merged list of loaded and cached data
-            Action<Collection<EventPage>> persistWorker = pages =>
-            {
-                // remove all pages which status is "trash"
-                var itemsToRemove = pages.Where(x => x.Status == "trash").ToList();
-                foreach (var page in itemsToRemove)
-                {
-                    pages.Remove(page);
                 }
             };
 
             return DataLoaderProvider.ExecuteLoadMethod(forceRefresh, this,
-                () => _dataLoadService.GetEventPages(forLanguage, forLocation, new UpdateTime(LastUpdated.Ticks)),
-                errorLogAction, worker, persistWorker);
+                () => _dataLoadService.GetEventPages(forLanguage, forLocation),
+                errorLogAction, worker);
         }
     }
 }
