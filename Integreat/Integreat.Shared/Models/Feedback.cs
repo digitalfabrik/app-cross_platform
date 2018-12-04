46a9d39531164dd21159e24b4b624ce907d0bcf2
Vincent Gierisch <vincentgierisch@gmail.com>
2018-12-04 21:52:47 +0100

    Add feedback-model

---
+++ Integreat/Integreat.Shared/Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Integreat.Shared.Models
+{
+    public class Feedback
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("permalink")]
+        public string Permalink { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+    }
+}
