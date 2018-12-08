4cebf1cfc64fd5b69bc014f2bc3f9cb8a37f11db
Vincent Gierisch <vincentgierisch@gmail.com>
2018-12-08 02:38:11 +0100

    add DataSender classes

---
+++ Integreat/Integreat.Shared/Data/Sender/Targets/FeedbackDataSender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Integreat.Shared.Data.Loader;
+using Integreat.Shared.Models;
+
+namespace Integreat.Shared.Data.Sender.Targets
+{
+    public class FeedbackDataSender : IDataSender
+    {
+        private readonly IDataLoadService _dataLoadService;
+        public string FileName => throw new NotImplementedException();
+
+        public FeedbackDataSender(IDataLoadService dataLoadService) {
+            _dataLoadService = dataLoadService;
+        }
+
+        public Task Send(Language language, Location location, Feedback feedback, Action<string> errorLogAction = null) {
+            return DataSenderProvider.ExecuteSendMethod(this, () => _dataLoadService.SendFeedback(feedback, language, location), errorLogAction);
+        }
+    }
+}
