fc7e9a72a275907481ce607489629cae93efc8a9
Vincent Gierisch <vincentgierisch@gmail.com>
2018-12-12 19:01:42 +0100

    add custom feedback view to project

---
+++ Integreat/Integreat.Shared/Views/FeedbackView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Integreat.Shared.Views
+{
+    public partial class FeedbackView : ContentView
+    {
+        public FeedbackView()
+        {
+            InitializeComponent();
+        }
+    }
+}
