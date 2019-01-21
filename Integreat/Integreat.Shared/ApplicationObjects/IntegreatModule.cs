791f3469b05a5583bc56b784961839a54f7334ee
Simple-codinger <vincentgierisch@gmail.com>
2019-01-21 02:13:13 +0100

    register FeedbackViewModel

--- Integreat/Integreat.Shared/ApplicationObjects/IntegreatModule.cs
+++ Integreat/Integreat.Shared/ApplicationObjects/IntegreatModule.cs
@@ -109,6 +109,7 @@ namespace Integreat.Shared.ViewFactory
             builder.RegisterType<LocationsViewModel>();
             builder.RegisterType<LanguagesViewModel>(); // can have multiple instances
 
+            builder.RegisterType<FeedbackViewModel>();
             builder.RegisterType<FeedbackDialogViewModel>();
 
             builder.RegisterType<SearchViewModel>();
