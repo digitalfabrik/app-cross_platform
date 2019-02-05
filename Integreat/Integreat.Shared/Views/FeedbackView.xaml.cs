8a86597ee132b2ddb56dee48967808e05878c91f
Simple-codinger <vincentgierisch@gmail.com>
2019-02-05 02:32:35 +0100

    remove unused usings

--- Integreat/Integreat.Shared/Views/FeedbackView.xaml.cs
+++ Integreat/Integreat.Shared/Views/FeedbackView.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using Autofac;
-using Integreat.Shared.Pages.Feedback;
-using Integreat.Shared.ViewFactory;
+﻿using Autofac;
 using Integreat.Shared.ViewModels;
-using Rg.Plugins.Popup.Pages;
-using Rg.Plugins.Popup.Services;
 
 namespace Integreat.Shared.Views
 {
