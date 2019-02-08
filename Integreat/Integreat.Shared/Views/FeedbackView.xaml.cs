ddd246394b95d5379f13120056dab8f99cb426fd
Simple-codinger <vincentgierisch@gmail.com>
2019-02-08 03:26:41 +0100

    add bindable Feedbacktype property

--- Integreat/Integreat.Shared/Views/FeedbackView.xaml.cs
+++ Integreat/Integreat.Shared/Views/FeedbackView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Autofac;
+using Integreat.Shared.Utilities;
 using Integreat.Shared.ViewModels;
+using Xamarin.Forms;
 
 namespace Integreat.Shared.Views
 {
@@ -10,5 +12,18 @@ namespace Integreat.Shared.Views
             this.BindingContext = IntegreatApp.Container.Resolve<FeedbackViewModel>();
             InitializeComponent();
         }
+
+        public static readonly BindableProperty FeedbackTypeProperty = BindableProperty.Create(
+                                nameof(FeedbackType),
+                                typeof(FeedbackType),
+                                typeof(FeedbackView),
+                                15);
+
+        public FeedbackType FeedbackType
+        {
+            get => (FeedbackType)GetValue(FeedbackTypeProperty);
+            set => SetValue(FeedbackTypeProperty, value);
+        }
+
     }
 }
