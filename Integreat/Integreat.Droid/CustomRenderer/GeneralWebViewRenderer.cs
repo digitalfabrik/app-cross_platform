26c12eb4e413b6d652eda649fd29cfd2506d9903
Simple-codinger <vincentgierisch@gmail.com>
2019-04-03 15:37:51 +0200

    Add webview renderer to block error page from being opened

--- Integreat/Integreat.Droid/CustomRenderer/GeneralWebViewRenderer.cs
+++ Integreat/Integreat.Droid/CustomRenderer/GeneralWebViewRenderer.cs
@@ -1,19 +1,76 @@
-﻿using Android.Content;
-using Integreat.Droid.CustomRenderer;
+﻿using System;
+using Android.Content;
+using Android.Webkit;
+using AWebView = Android.Webkit.WebView;
+using System.Threading.Tasks;
+using Android.Graphics;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Integreat.Droid.CustomRenderer;
 
-[assembly: ExportRenderer(typeof(WebView), typeof(GeneralWebViewRenderer))]
+[assembly: ExportRenderer(typeof(Xamarin.Forms.WebView), typeof(GeneralWebViewRenderer))]
 namespace Integreat.Droid.CustomRenderer
 {
     /// <summary>
     /// Custom render for WebViews with zoom possibility
     /// </summary>
     /// <seealso cref="Xamarin.Forms.Platform.Android.WebViewRenderer" />
-    public class GeneralWebViewRenderer : ZoomingWebViewRenderer
+    public class GeneralWebViewRenderer : WebViewRenderer
     {
+        IWebViewController ElementController => Element;
+
         public GeneralWebViewRenderer(Context context) : base(context)
         {
+        }
 
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetWebViewClient(new WebClient(this));
+            }
+        }
+
+        class WebClient : WebViewClient
+        {
+            GeneralWebViewRenderer _renderer;
+
+            public WebClient(GeneralWebViewRenderer renderer)
+            {
+                _renderer = renderer ?? throw new ArgumentNullException("renderer");
+            }
+
+            public override bool ShouldOverrideUrlLoading(AWebView view, string url)
+            {
+                if (_renderer.Element == null)
+                    return true;
+
+                var args = new WebNavigatingEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource { Url = url }, url);
+
+                _renderer.ElementController.SendNavigating(args);
+
+                _renderer.UpdateCanGoBackForward();
+                return args.Cancel;
+            }
+
+            public override void OnPageFinished(AWebView view, string url)
+            {
+                base.OnPageFinished(view, url);
+
+                var source = new UrlWebViewSource { Url = url };
+                var args = new WebNavigatedEventArgs(WebNavigationEvent.NewPage, source, url, WebNavigationResult.Success);
+                _renderer.ElementController.SendNavigated(args);
+            }
+
+            public override void OnPageStarted(AWebView view, string url, Bitmap favicon)
+            {
+                base.OnPageStarted(view, url, favicon);
+
+                var args = new WebNavigatingEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource { Url = url }, url);
+                _renderer.ElementController.SendNavigating(args);
+            }
         }
     }
 }
